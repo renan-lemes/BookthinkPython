cba1a2af586a81266aae3f8ada9922180432de95
RenanLemes <renan_ll@hotmail.com>
2022-08-01 22:16:40 -0300

    add: novas praticas do livro

---
+++ README.md
@@ -0,0 +1,11 @@
+## Livo Pense em Python 
+* Capitulo 1 - Depurar em python
+* Capitulo 2 - Variaveis e instruções
+* Capitulo 3 - Funções
+* Capitulo 4 - Projeto de interface
+    1- Comece escrevendo um pequeno programa sem definições de funções
+    2- Uma vez que o programa esteja funcionando indentifique uma parte coerente dele, encapsule essa parte em uma função e dê um nome a ela.
+    3- Generalize a função acrescentado os parâmetros adequados.
+    4- Repita os passos 1-3 até que tenha um conjunto de funções operantes.
+    5- procure oportunidades de melhorar o programa pela refatoração. Por exemplo, se você tem um código semelhante em vários lugares, pode ser uma boa ideia fatora-lo em uma função geral.
+*
--- mypolygon.py
+++ mypolygon.py
@@ -2,6 +2,9 @@
 import turtle
 import math
 
+
+
+
 bob = turtle.Turtle()
 
 def square(bob, length):
@@ -23,8 +26,32 @@ def circle(bob, length, n, r):
         bob.fd(length)
         bob.lt(angle)
 
+# solução do livro 
+def arc(t,r,angle):
+    arc_lenght = 2*math.pi*angle/360
+    n = int(arc_lenght/3)+1
+    step_length = arc_lenght/n
+    step_angle = angle/n
+    for i in range(n):
+        t.fd(step_length)
+        t.lt(step_angle)
+
+def polyline(t, n, lenght, angle):
+    for i in range(n):
+        t.fd(lenght)
+        t.lt(angle)
 
 
+
+def arc2(t,r,angle):
+    arc_length = 2*math.pi*r*angle/360
+    n = int(arc_length/3)+1
+    step_length = arc_length/n
+    step_angle = float(angle)/n
+    polyline(t,n,step_length, step_angle)
+
+def cicle2(t,r):
+    arc2(t,r,360)
 
 """ 
 bob.fd(100)
@@ -38,8 +65,9 @@ bob.fd(100) """
 
 #square(bob, 199)
 #poligon(bob, 100, 6)
-#circle(bob, 100, 80, 30)
-
+circle(bob, 100, 80, 30)
+#arc(bob, 100, 120)
+#cicle2(bob, 30)
 
 
 
