345de36b2c0bf52d54dacfa6b80640cb82313f89
RenanLemes <renan_ll@hotmail.com>
2022-11-02 12:53:43 -0300

    Soluções do exercicio de fatorial e distancia

--- Solutions/is_between.py
+++ Solutions/is_between.py
@@ -1,4 +1,20 @@
+
 import math
+
+def factorial(n:int):
+    if type(n) != int:
+        print('Factorial is only definined for integers.')
+        return None
+    elif n < 0:
+        print('Factorial is not defined negative integers.')
+        return None
+    elif n == 0:
+        return 1
+    else:
+        recurse = factorial(n-1)
+        result = n * recurse
+        return result
+
 
 def distance_r2(x1, x2, y1,y2):
     return math.sqrt((x2-x1)**2  +  (y2-y1)**2)
@@ -9,7 +25,7 @@ def is_betwenn(x, y, z):
     else:
         return False
 
-
+print(factorial(2))
 print(is_betwenn(1, 1 ,4))
 print(distance_r2(2,2,1,3))
 
