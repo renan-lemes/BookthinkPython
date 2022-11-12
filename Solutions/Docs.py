b81492a10d7fb235afd728b1b557a7935f196579
RenanLemes <renan_ll@hotmail.com>
2022-11-12 10:41:31 -0300

    Add:Estudando alguns metodos de strings e alguns matematicos

---
+++ Solutions/Docs.py
@@ -0,0 +1,33 @@
+''' 
+    Alguns métodos Matemáticos Interesantes.
+
+'''
+x = 2
+y = 4
+z = 0.4213
+
+b = 2.0
+
+c = complex(x,y)
+
+c_conj = c.conjugate()
+
+mod_d = divmod(x,y)
+
+e = pow(x, y)
+
+e2 = x**y
+
+modl = abs(x)
+
+flo = round(z, 2)
+
+f = b.is_integer()
+
+print(f)
+
+
+''' 
+    Alguns Métodos de strings 
+'''
+
