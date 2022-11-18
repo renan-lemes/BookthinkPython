e353ebd506e17cf07e983ab486994b20e4ce0e85
RenanLemes <renan_ll@hotmail.com>
2022-11-17 21:15:54 -0300

    add:adicionando alguns estudos alguns metodos estudados

--- Solutions/Docs.py
+++ Solutions/Docs.py
@@ -22,9 +22,42 @@ modl = abs(x)
 
 flo = round(z, 2)
 
-f = b.is_integer()
+f = z.is_integer() ## verifica se tem um numero inteiro ou não 
 
 print(f)
+
+''' 
+    Alguns Métodos de objetos
+'''
+
+def P_I(n = 0):
+    p_i = 0
+    while True:
+        p_i += ((-1)**n) / (2*n + 1)
+        if n > 900:
+            break
+        n += 1
+    return  4* p_i
+
+print(P_I(n=0))
+
+''' 
+    Métoodos de Conjuntos
+'''
+A = set({1, 2,4})
+B = set({1,8,4})
+
+t = A == B
+
+i = A.intersection(B)
+u = A.union(B)
+d = A.difference(B)
+d_ = A.symmetric_difference(B)
+
+print(i)
+print(u)
+print(d)
+print(d_)
 
 
 ''' 
