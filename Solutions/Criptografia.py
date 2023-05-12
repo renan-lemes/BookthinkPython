45979f897d7c6222c320cfd23be668d4dd6ee21e
RenanLemes <renan_ll@hotmail.com>
2023-05-11 21:31:26 -0300

    Chores:colocando a solução do Allen pra quem quiser ver

--- Solutions/Criptografia.py
+++ Solutions/Criptografia.py
@@ -18,24 +18,49 @@ def rotate_word(palavra, num):
 
 print(rotate_word('dale', 25))
 
-# palavra = 'palavrasenha'
-# #palavra :  
+####################################################
+##             solução do Allen Downey
+####################################################
 
 
-# cripto = []
+from __future__ import print_function, division
 
-# senha = ''
 
-# base = 3
+def rotate_letter(letter, n):
+    """Rotates a letter by n places.  Does not change other chars.
 
-# #print(letras.find('a'))
+    letter: single-letter string
+    n: int
 
-# for i in range(len(palavra)):
+    Returns: single-letter string
+    """
+    if letter.isupper():
+        start = ord('A')
+    elif letter.islower():
+        start = ord('a')
+    else:
+        return letter
 
-#     # if ((letras.find(palavra[i]) + 3) > 25 ):
-#     cripto.append(letras[letras.find(palavra[i]) + 3])
+    c = ord(letter) - start
+    i = (c + n) % 26 + start
+    return chr(i)
 
-# for i in range(len(cripto)):
-#     senha += cripto[i]
 
-# print(senha)
+def rotate_word(word, n):
+    """Rotates a word by n places.
+
+    word: string
+    n: integer
+
+    Returns: string
+    """
+    res = ''
+    for letter in word:
+        res += rotate_letter(letter, n)
+    return res
+
+
+if __name__ == '__main__':
+    print(rotate_word('cheer', 7))
+    print(rotate_word('melon', -10))
+    print(rotate_word('sleep', 9))
