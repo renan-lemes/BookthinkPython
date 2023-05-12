868f73fa7ada7b8c544f16a3a18b4864bc65d171
RenanLemes <renan_ll@hotmail.com>
2023-05-11 21:24:36 -0300

    Chores:Finalizando a parte de criptografia

--- Solutions/Criptografia.py
+++ Solutions/Criptografia.py
@@ -1,23 +1,41 @@
 
+def rotate_word(palavra, num):
+    alfabeto = 'abcdefghijklmnopqrstuvxwyz'
+    cripto = list()
+    for i in range(len(palavra)):
+        if alfabeto.find(palavra[i])+num > len(alfabeto):
+            resto = alfabeto.find(palavra[i])+num - len(alfabeto)
+            cripto.append(alfabeto[resto])
+        else:
+            cripto.append(alfabeto[alfabeto.find(palavra[i])+num])
+    p = ''
+    for k in cripto:
+        p += f'{k}'
+    
+    return p
 
-letras = 'abcdefghijklmnopqrstuvxwyz'
 
-palavra = 'palavrasenha'
 
-cripto = []
+print(rotate_word('dale', 25))
 
-senha = ''
+# palavra = 'palavrasenha'
+# #palavra :  
 
-base = 3
 
-#print(letras.find('a'))
+# cripto = []
 
-for i in range(len(palavra)):
+# senha = ''
 
-    #if ((letras.find(palavra[i]) + 3) > 25 ):
-    cripto.append(letras[letras.find(palavra[i]) + 3])
+# base = 3
 
-for i in range(len(cripto)):
-    senha += cripto[i]
+# #print(letras.find('a'))
 
-print(senha)
+# for i in range(len(palavra)):
+
+#     # if ((letras.find(palavra[i]) + 3) > 25 ):
+#     cripto.append(letras[letras.find(palavra[i]) + 3])
+
+# for i in range(len(cripto)):
+#     senha += cripto[i]
+
+# print(senha)
