71fa4b219226c89bd7d82f1a37aa2d2dcf714667
RenanLemes <renan_ll@hotmail.com>
2023-07-23 22:37:19 -0300

    Chores:teste de listas

--- Solutions/List.py
+++ Solutions/List.py
@@ -49,4 +49,40 @@ lista_a.remove('opa') ## aqui o remove colocamos o value
 
 print(lista_a)
 
+ls = [1, 2, 4, 2,6,8]
+lsList = ls
+
+del ls[0]
+print(ls)
+
+del lsList[1:6] ## nunca exclui o ultimo elemento ...
+print(lsList)
+
+st = 'spawn'
+
+print(list(st))
+
+def tail (t):
+    return t[1:]
+
+print(tail(st))
+
+ls = [4, 5, 12,  1, 0]
+
+ls.sort()
+print(ls)
+
+''' 
+    t.append(x)
+    t = t + [x]
+    t += [x]
+
+    Errado !!!!
+    t.append([x])
+    t = t.append(x)
+    t + [x]
+    t = t + x
+
+'''
+#   sorted() retorna uma lista reordenada 
 
