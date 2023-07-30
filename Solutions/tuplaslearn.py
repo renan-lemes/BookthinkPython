e4e79b9fff558b844f7d20cfce95a70207d95f0d
RenanLemes <renan_ll@hotmail.com>
2023-07-30 17:05:20 -0300

    Chores:resolvendo os exercicios de struct

--- Solutions/tuplaslearn.py
+++ Solutions/tuplaslearn.py
@@ -20,7 +20,7 @@ t = (4,9)
 
 def sumall(*args)-> int or float:
     '''
-        Função que retorna a soma de qualquer tipo de dados que entrar na função  
+        Função que retorna a soma de qualquer tipo de dados
     '''
     print(type(*args))
     soma = 0
@@ -32,8 +32,63 @@ def sumall(*args)-> int or float:
 
     return soma
 
-a = sumall((1,2,4,6))
+# a = sumall((1,2,4,6))
 
 # print(a)
 
 ## Bacana uma lista de tuplas pode ser convertido em dict e o mesmo acontece ao contrario
+
+## Exercicio 12.1 ✅ ## precisei da ajuda do autor aqui 
+
+def most_freq(s:str):
+    hist = histogram(s)
+
+    t = []
+    for x, freq in hist.items():
+        t.append((freq,x))
+
+    t.sort(reverse=True)
+
+    res = list()
+    for freq,x in t:
+        res.append(x)
+
+    return res 
+
+
+def histogram(s:str)->dict:
+    hist = dict()
+    for x in s:
+        hist[x] = hist.get(x, 0) +1
+    
+    return hist
+
+
+def read_file(filename):
+    with open(filename, 'r') as arquivo:
+        arq = arquivo.read()
+
+    return arq    
+
+words = read_file('Solutions\\words.txt')
+
+# print(words) ## temos todas as palacras do words.txt
+
+# print(histogram(words))
+# print(most_freq(words))
+
+## Exericio 13.5 ✅  
+
+def prob_hist(s:dict)->dict:
+    total = 0
+    prob_d = {}
+    total = sum([x[1] for x in s.items()])
+    
+    for key, value in s.items():
+        prob_d[key] = value / total
+    return prob_d
+
+print(prob_hist(histogram(['a', 'a', 'b'])))
+
+
+
