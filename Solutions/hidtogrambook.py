a1744f9b9acf478cfac16c63e683baf393ebe476
RenanLemes <renan_ll@hotmail.com>
2023-07-30 17:43:11 -0300

    Chores:adicionando mais codigos vistos no livro

--- Solutions/hidtogrambook.py
+++ Solutions/hidtogrambook.py
@@ -1,5 +1,5 @@
 import string
-
+import random
 ''' 
     O codigo faz um histograma com palavra de um livro adicionando um contador para cada palavra falada
     Interesando para modelos NLP. 
@@ -22,3 +22,33 @@ def preocess_file (filename:str) -> dict:
     return hist
 
 print(preocess_file('Solutions\\words.txt'))
+
+def most_common(hist):
+    t = []
+    for key, value in hist.items():
+        t.append((value, key))
+    t.sort(reverse=True)
+    return t
+
+def print_most_common(hist, num = 10):
+    t = most_common(hist)
+    print('As palavras com mais frequencia sao')
+    for freq, word in t[:num]:
+        print(word, freq)
+
+def substract (d1, d2):
+    res = dict()
+    for key in d1:
+        if key not in d2:
+            res[key] = None
+    return res
+
+def ramdom_hist (hist:dict) ->list:
+    ''' 
+        Função para retornar uma lista aleatoria de de palavras em um dicionario 
+    '''
+    t = []
+    for word, freq in hist.items():
+        t.extend([word] * freq) ## extend de uma lista é um tipo de concat também 
+    return random.choice(t)
+
