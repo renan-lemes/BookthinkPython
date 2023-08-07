15a0eda6f965c78c782603af3dcf816123a95cba
RenanLemes <renan_ll@hotmail.com>
2023-08-07 17:55:55 -0300

    Chores:arrumando a parte 2

--- Solutions/structdata.py
+++ Solutions/structdata.py
@@ -1,6 +1,10 @@
 ## Exercicio 13.8 Analise de markov ✅
 from hitogrambook import *
 
+import random
+
+from random import choice
+# 1
 def Markov (dict_txt:dict):
     prefix = {}
     unfix = {}
@@ -17,8 +21,44 @@ def Markov (dict_txt:dict):
 def read_file (name:str)-> dict:
     dict_str =  preocess_file(name)
     prefix, unfix = Markov(dict_str)
-    print(prefix)
-    print(unfix)
+    
+    # print(prefix)
+    # print(unfix)
+    
+    return dict_str, prefix, unfix
 
 
-read_file('Solutions\\book.txt')
+# read_file('Solutions\\book.txt')
+
+
+## Parte 2
+
+def frase_aleatoria()->str:
+    dc_text ,prefix, sufix = read_file('Solutions\\text.txt')
+    
+    # print(dc_text)
+    _frs = ""
+    for i in range(7):
+        word = choice(list(dc_text.keys()))
+        del dc_text[word]
+
+        num_ram = random.randint(0,2)
+        if num_ram == 0 :
+            pr = choice(list(prefix))
+            del prefix[pr]
+            _frs += " " + word+pr +" "
+        elif num_ram == 1:
+            ix = choice(list(sufix))
+            del sufix[ix]
+            _frs += " " + word+ix +" "
+        else :
+            _frs += "" + word +" "
+            
+    return _frs
+
+    # print(choice(list(dc_text.keys())))
+
+
+
+
+print(frase_aleatoria())
