060c92e75174d27ec9e2dbf770d51c98a0b19582
RenanLemes <renan_ll@hotmail.com>
2023-09-11 23:15:13 -0300

    Chores:tem um erro na minha class parece que so tem as de espadas

--- Solutions/Classex.py
+++ Solutions/Classex.py
@@ -1,3 +1,4 @@
+import random 
 ## Exercicios do cap 18
 ## Ec 18.1 ✅
 ## Ex 18.2
@@ -13,9 +14,69 @@ class Card :
     suit_names = ['Paus', 'Ouros', 'Copas', 'Espadas']
     rank_name = [None, 'As', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valete', 'Dama', 'Rei']
     def __str__(self) -> str:
-        return '%s de %s' % (Card.rank_name[self.rank], Card.suit_names[self.suit]) 
+        return '%s de %s' % (Card.rank_name[self.rank], Card.suit_names[self.suit])
+    
+    def __lt__(self, other):
+        ## para conferir os naipes
+        if self.suit < other.suit: return True
+        if self.suit > other.suit: return False
+
+        ## Comparacao de tuplas
+        t1 = self.suit, self.rank
+        t2 = other.suit, other.rank
+        return t1 < t2
+
+class Deck:
+    ''' 
+        Inicializando meu deck aqui 
+    '''
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+    def __str__(self) -> str:
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+    
+    def pop_card(self):
+        return self.cards.pop()
+    
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def suffle(self):
+        ''' 
+            Pegar uma carta aleatoria
+        '''
+        random.shuffle(self.cards)
+
+class Hand(Deck):
+    '''
+        Representa a mao do jogador
+    '''
+    def __init__(self, label=''):
+        self.cards = []
+        self.label = label
+
+
+
+
+
 
 ## exemplo 
-queen_diamonds = Card(1, 12)
+# queen_diamonds = Card(1, 12)
 
-print(queen_diamonds)
+# print(queen_diamonds)
+
+hand = Hand('new hand')
+deck = Deck()
+
+card = deck.pop_card()
+card2 = deck.pop_card()
+hand.add_card(card)
+hand.add_card(card2)
+print(hand)
